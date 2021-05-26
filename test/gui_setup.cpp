09b54c5ec45ede876e7094ae08530fa84b31076a
Some0neUnknown <vladimir-malyschevvv@mail.ru>
2021-05-26 14:15:19 +0700

    Made building tests in CI

--- test/gui_setup.cpp
+++ test/gui_setup.cpp
@@ -13,7 +13,8 @@ CTEST(gen_template_list, selection_list)
     dictionary.push_back("Не знаю");
     dictionary.push_back("Скорее да, чем нет");
     dictionary.push_back("Да");
-    std::vector<JButton> selection_list = generate_template_list(font, dictionary);
+    std::vector<JButton> selection_list
+            = generate_template_list(font, dictionary);
     sf::Text a(draw_russian(dictionary[0]), font, 15);
     bool result = a.getString() == selection_list[0].button_text.getString();
     ASSERT_TRUE(result);
